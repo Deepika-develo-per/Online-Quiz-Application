f35df32c4268a41d4f7c8e9fbb986cb39a9fa2f6
Deepika-develo-per <sssdeepika12@gmail.com>
2024-08-05 19:42:46 +0530

    initial commit

---
+++ src/main/java/com/example/demo/SpringBootOnlineQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnlineQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOnlineQuizApplication.class, args);
+	}
+	
+}
